d72aa5c604a5758ab62869f7e7e41355b6cb280b
sherif <sherifelgendy2004@gmail.com>
2025-05-01 15:45:23 +0300

    department-relationship-with-doctor

--- app/models/department.py
+++ app/models/department.py
@@ -3,6 +3,7 @@ from typing import List
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from base_model import BaseModel
+from doctor import Doctor
 from medical_device import MedicalDevice
 from room import Room
 
@@ -20,4 +21,7 @@ class Department(BaseModel):
         "MedicalDevice",
         back_populates="department",  # Must match MedicalDevice's relationship name
         cascade="all, delete-orphan",  # Optional: auto-delete devices if department is deleted
+    )
+    doctors: so.Mapped[List["Doctor"]] = so.relationship(
+        "Doctor", back_populates="department", cascade="all, delete-orphan"
     )
