8ac18470411297c75ae939042a884c442a8302e8
Mazen Atlam <mazenatef5510@gmail.com>
2025-05-02 09:37:21 +0300

    comment init_load_db clause

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from app import create_app, db
 
 app = create_app()
 
-with app.app_context():
-    from app.utils.init_load_db import init_load_db
-
-    init_load_db(db)
+# with app.app_context():
+#     from app.utils.init_load_db import init_load_db
+#
+#     init_load_db(db)
 
 if __name__ == "__main__":
     app.run(debug=True)
