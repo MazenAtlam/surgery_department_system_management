4458fe3b27290d9c6f82b661553d5989d4ecc084
Mazen Atlam <mazenatef5510@gmail.com>
2025-05-03 01:20:10 +0300

    complete authentication and a query to select all fields from a user using id

--- app/routes/v1/user.py
+++ app/routes/v1/user.py
@@ -5,7 +5,7 @@ from app.services.user_service import get_user_data_by_id
 user_bp = Blueprint("user", __name__)
 
 
-@user_bp.route("/<int:user_id>", methods=["GET"])
+@user_bp.route("/<string:user_id>", methods=["GET"])
 def get_user(user_id):
     user_data = get_user_data_by_id(user_id)
     if user_data:
